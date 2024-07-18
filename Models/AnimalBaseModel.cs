311bbfcb89c34f8bdfd136020436809c0d228867
AnjoYT <AnjoYT@wp.pl>
2024-07-18 15:15:27 +0200

    Adding AnimalModel and Sex enum

--- Models/AnimalBaseModel.cs
+++ Models/AnimalBaseModel.cs
@@ -1,10 +1,11 @@
 ﻿namespace RackManager.Models
 {
-    public class AnimalModel
+    public class AnimalBaseModel
     {
         public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string Image { get; set; }
         public float Weight { get; set; }
-
+        public enum Sex { get;set; }
     }
 }
