d5655e6e7a8fc88228d830d4c778dfa7b43e4160
AnjoYT <AnjoYT@wp.pl>
2024-10-16 19:47:11 +0200

    Creating basic template selector for button modules

---
+++ CustomControls/ButtonTemplateSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RackManager.CustomControls
+{
+    class ButtonTemplateSelector : DataTemplateSelector
+    {
+        //FUTURE NOTE: create generalized version for button and card selectors
+        public DataTemplate versionOne { get; set; }
+        public DataTemplate versionTwo { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is not null)
+            {
+                return versionOne;
+            }
+            else
+            {
+                return versionTwo;
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
