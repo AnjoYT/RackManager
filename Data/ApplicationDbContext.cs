efa7e5287861b2a53bacf9458365e7806559bce4
AnjoYT <AnjoYT@wp.pl>
2024-10-25 19:15:24 +0200

    configuring DbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RackManager.Entities;
+using System.Configuration;
+
+namespace RackManager.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<ImageEntity> Images { get; set; }
+        public DbSet<AnimalBaseEntity> Animals { get; set; }
+        public DbSet<SnakeEntity> Snakes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ImageEntity>()
+                .ToTable("Animal")
+                .HasDiscriminator<string>("Animals_type")
+                .HasValue<ImageEntity>("Image")
+                .HasValue<AnimalBaseEntity>("Animal_base")
+                .HasValue<SnakeEntity>("Snake");
+            modelBuilder.Entity<HumidityEntity>();
+            modelBuilder.Entity<TempEntity>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
