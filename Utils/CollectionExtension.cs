6d1bff7ce1efe7e306bf2fe84012515cbea36c53
AnjoYT <AnjoYT@wp.pl>
2024-11-02 20:53:13 +0100

    Creating extension to convert IEnum to Observable object

---
+++ Utils/CollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RackManager.Utils
+{
+    public static class CollectionExtension
+    {
+        public static ObservableCollection<T> ToCollection<T>(this ObservableCollection<T> collection, IEnumerable<T> list)
+        {
+            if (collection != null)
+            {
+                foreach (T item in list)
+                {
+                    collection.Add(item);
+                }
+                return collection;
+            }
+            return null;
+        }
+    }
+}
