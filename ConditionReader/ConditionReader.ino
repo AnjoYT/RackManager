b2410bd2b355fead68f93831e68af945955ea395
AnjoYT <AnjoYT@wp.pl>
2024-11-12 19:15:58 +0100

    refactoring main program to use seperated class

--- ConditionReader/ConditionReader.ino
+++ ConditionReader/ConditionReader.ino
@@ -1,19 +1,19 @@
 
 #include "DHT11.h"
-#include "DS18B20.h"
+#include "TemperatureController.h"
 
-#define DHT_PIN D5
+#define DHT11_PIN D5
 #define DS18B20_PIN D4
 
-DHT11 dht11(DHT_PIN);
-DS18B20 ds18b20(DS18B20_PIN);
+DHT11 dht11(DHT11_PIN);
+TemperatureController temperatureController(DS18B20_PIN);
 
 void setup() {
     Serial.begin(9600);
 }
 
 void loop() {
-
+/*
   int humidity = dht11.readHumidity();
       if (humidity != DHT11::ERROR_CHECKSUM && humidity != DHT11::ERROR_TIMEOUT) {
         Serial.print("Humidity");
@@ -25,4 +25,6 @@ void loop() {
         Serial.print(ds18b20.getTempC());
         Serial.println(";");
       }
+      */
+      Serial.println(temperatureController.ReadTemperature());
 }
