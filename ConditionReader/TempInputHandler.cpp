f81ef4b0be52accfd7df9a420477714c77d2cef7
AnjoYT <AnjoYT@wp.pl>
2024-11-19 20:12:40 +0100

    Adding functionality for class to validate and return min and bax value

---
+++ ConditionReader/TempInputHandler.cpp
@@ -0,0 +1,71 @@
+#include "Arduino.h"
+#include <string>
+#include "TempInputHandler.h"
+TempInputHandler::TempInputHandler(Stream& inputStream):_inputStream(inputStream){}
+MinMax TempInputHandler::GetInput()
+{
+  if(_inputStream.available()>0){
+      inputValue = GetFloatInput();
+      return inputValue;
+  }
+  return MinMax();
+}
+MinMax TempInputHandler::GetFloatInput()
+{
+  std::string input = _inputStream.readStringUntil('\n').c_str();
+  if(!input.empty()&&input.back()==';')
+  {
+  return ParseMinMax(input);
+  }
+  return MinMax();
+}
+
+bool TempInputHandler::IsFloat(std::string input){
+  char* end;
+  std::strtod(input.c_str(),&end);
+  return (*end == '\0');
+}
+
+MinMax TempInputHandler::ParseMinMax(std::string input){
+  MinMax minMaxTemp;
+  std::vector<std::string> valueNotParsed = Split(input,';');
+  if(valueNotParsed.size()!=2)
+  {
+    return MinMax();
+  }
+  if(IsFloat(valueNotParsed[0])&&IsFloat(valueNotParsed[1]))
+  {
+    if(std::stof(valueNotParsed[0])<=std::stof(valueNotParsed[1]))
+    {
+      minMaxTemp.minTemp = std::stof(valueNotParsed[0]);
+      minMaxTemp.maxTemp = std::stof(valueNotParsed[1]);
+    }else
+    {
+      minMaxTemp.minTemp = std::stof(valueNotParsed[1]);
+      minMaxTemp.maxTemp = std::stof(valueNotParsed[0]);
+    }
+    
+  }
+  else {
+    return MinMax();
+  }
+  return minMaxTemp;
+  
+}
+
+std::vector<std::string> TempInputHandler::Split(std::string input,char delimiter)
+{
+  std::vector<std::string> values;
+  size_t pos = 0;
+  while((pos = input.find(delimiter)) != std::string::npos)
+  {
+    std::string value;
+    
+    value = input.substr(0,pos);
+    values.push_back(value);
+    input.erase(0,pos+1);
+  }
+  return values;
+}
+
+
